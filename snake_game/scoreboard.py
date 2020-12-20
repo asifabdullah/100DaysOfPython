c796cda7aabdcfc1f28b0dd3b6cf44d7ce77acb8
Asif Abdullah <asif.abdullah@mutualtrustbank.com>
2020-12-20 15:02:59 +0600

    Day 24 Project has done

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = int(self.fetch_high_score())
         self.hideturtle()
         self.pencolor("white")
         self.penup()
@@ -15,13 +16,29 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", move=False, align=ALIGN, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score}, High Score: {self.high_score}", move=False, align=ALIGN, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="Game Over", move=False, align=ALIGN, font=FONT)
+    def reset_scoreboard(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.update_high_score()
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(arg="Game Over", move=False, align=ALIGN, font=FONT)
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_scoreboard()
+
+    def fetch_high_score(self):
+        with open("data.txt") as file:
+            high_score = file.read()
+            return high_score
+
+    def update_high_score(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.high_score))
