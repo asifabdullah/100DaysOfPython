d58158c276f496ece5f6dfc45265a63cb50dd187
Md Asif Abdullah <asifabdullah@Mds-MacBook-Pro.local>
2020-12-21 02:55:06 +0600

    Day 26 Done

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -17,10 +17,10 @@ guessed_list = []
 while len(guessed_list) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_list)}/50 States Correct", prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_list:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_list]
+        # for state in all_states:
+        #     if state not in guessed_list:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("state_to_learn.csv")
         break
