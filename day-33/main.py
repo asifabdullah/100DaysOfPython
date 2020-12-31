49a6830f59428fb75a7fefbd0d27e84052e82473
asifabdullah <asif.ndc@gmai.com>
2020-12-31 12:38:30 +0600

    This is a test

--- day-33/main.py
+++ day-33/main.py
@@ -9,10 +9,22 @@ response.raise_for_status()
 
 data = response.json()
 
-longitude = float(data["iss_position"]["longitude"])
-latitude = float(data["iss_position"]["latitude"])
+# If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
 
-iss_position = (longitude, latitude)
+
+def if_iis_nearby(longitude, latitude):
+    if (longitude < MY_LNG - 5 or longitude < MY_LNG + 5) and (latitude < MY_LAT - 5 or latitude < MY_LAT + 5):
+        return True
+    return False
+
+
+iss_longitude = float(data["iss_position"]["longitude"])
+iss_latitude = float(data["iss_position"]["latitude"])
+
+iss_position = (iss_longitude, iss_latitude)
 print(iss_position)
 
 parameter = {
@@ -27,9 +39,8 @@ data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+time_now = datetime.now().hour
 
-print(data)
-print(sunrise)
-print(sunset)
-print(time_now.hour)
+if if_iis_nearby(latitude=iss_latitude, longitude=iss_longitude):
+    if time_now > sunset:
+        print()
