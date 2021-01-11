bb95d169ad8390170822fe3f9581887100abed88
Md Asif Abdullah <asifabdullah@Mds-MacBook-Pro.local>
2021-01-11 15:01:24 +0600

    This is a commit

--- Day35-RainAlert/main.py
+++ Day35-RainAlert/main.py
@@ -3,16 +3,16 @@ import requests
 from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 
-proxy_client = TwilioHttpClient()
-proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+# proxy_client = TwilioHttpClient()
+# proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
 # proxy_client = TwilioHttpClient(proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
 
 # This code block is required only for running in office
-proxyDict = {
-          'http': '192.168.51.61',
-          'https': '192.168.51.61'
-        }
+# proxyDict = {
+#           'http': '192.168.51.61',
+#           'https': '192.168.51.61'
+#         }
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "0d352bb3bfeda987d667d015ba4055d2"
@@ -27,9 +27,9 @@ weather_parameters = {
     "exclude": "current,minutely,daily,alerts"
 }
 # This one for office with proxy
-response = requests.get(url=OWM_Endpoint, params=weather_parameters, proxies=proxyDict)
+# response = requests.get(url=OWM_Endpoint, params=weather_parameters, proxies=proxyDict)
 # This one for home without proxy
-# response = requests.get(url=OWM_Endpoint, params=weather_parameters)
+response = requests.get(url=OWM_Endpoint, params=weather_parameters)
 response.raise_for_status()
 
 weather_data = response.json()
@@ -42,7 +42,8 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain:
-    client = Client(account_sid, auth_token, http_client=proxy_client)
+    # client = Client(account_sid, auth_token, http_client=proxy_client)
+    client = Client(account_sid, auth_token)
     message = client.messages \
         .create(
             body="It's going to rain today. Please bring an ☂",
